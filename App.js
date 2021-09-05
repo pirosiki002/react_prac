80512e72141c68d7a3c0061c88574dbd1f4a9b87
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-05 16:32:40 +0900

    L4-8

--- App.js
+++ App.js
@@ -2,54 +2,66 @@ import React, { useState} from 'react';
 import './App.css';
 
 function AlertMessage(props){
-
-  const data = ["Hello!", "Welcome...", "Good-bye?"];
-
-  const actionAlert = ()=>{
-    const re = data[Math.floor(Math.random() * data.length)];
-    props.setAlert('message:"' + re + '".');
-  }
+  const data = props.data;
+  const msg = JSON.stringify(data);
 
   return <div className="alert alert-primary h5 text-primary">
-    <h5>{props.alert}</h5>
-    <button onClick={actionAlert} className="btn btn-primary">
-      Click me!
-    </button>
-  </div>
-}
-
-function CardMessage(props){
-  const [count, setCount] = useState(0);
-
-  const actionCard = () =>{
-    setCount(count + 1);
-    props.setCard("card counter:" + count + "count.");
-  }
-
-  return <div className="card p-3 border-dark text-center">
-    <h5>{props.card}</h5>
-    <button onClick={actionCard} className="btn-secondary">
-      Click me!
-    </button>
+    <h5>{msg}</h5>
+    <hr />
+    <table className="table h6">
+      <tbody>
+        <tr><th>Name</th><td>{data.name}</td></tr>
+        <tr><th>Mail</th><td>{data.mail}</td></tr>
+        <tr><th>Age</th><td>{data.age}</td></tr>
+      </tbody>
+    </table>
   </div>
 }
 
 function App(){
-  const[alert, setAlert] = useState("This is alert message!");
-  const[card, setCard] = useState("This is Card message!");
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [age, setAge] = useState(0);
+  const [form, setForm] = useState({
+    name:'no name', mail:'no mail', age:0
+  });
+
+  const doChangeName = (event)=>{
+    setName(event.target.value);
+  }
+  const doChangeMail = (event)=>{
+    setMail(event.target.value);
+  }
+  const doChangeAge = (event)=>{
+    setAge(event.target.value);
+  }
+
+  const doSubmit = (event) => {
+    setForm({name:name, mail:mail, age:age});
+    event.preventDefault();
+  }
 
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <h4 className ="my-3">Hooks sample</h4>
-        <AlertMessage message={alert} setAlert={setAlert}/>
-        <CardMessage message={card} setCard={setCard}/>
-        <hr />
-        <div className="text-right">
-          <p>{alert}</p>
-          <p>{card}</p>
-        </div>
+        <h4 className="my-3">Hooks sample</h4>
+        <AlertMessage data={form} setData={setForm} />
+        <form onSubmit={doSubmit}>
+          <div className="form-group">
+            <lavel>Name:</lavel>
+            <input type="text" className = "form-control" onChange={doChangeName} />
+          </div>
+          <div className="form-group">
+            <lavel>Mail:</lavel>
+            <input type="text" className = "form-control" onChange={doChangeMail} />
+          </div>
+          <div className="form-group">
+            <lavel>Age:</lavel>
+            <input type="text" className = "form-control" onChange={doChangeAge} />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Click" />
+        </form>
       </div>
     </div>
   )

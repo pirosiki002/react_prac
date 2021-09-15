a46af96e392ac50297cb8a9866ab6640201fc227
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-15 12:03:04 +0900

    L5-4

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -6,27 +6,6 @@ export default function Home() {
   let title ="Next.js page";
   let message = "React Next.js sample page Da・yo・ne!.";
 
-  const h1 = {
-    textAlign:'right',
-    padding:'5px 15px'
-  }
-
-  const p = {
-    textAlign:'left',
-    margin:'0px 5px',
-    color:'#669',
-    fontSize:'18pt'
-  }
-
-  const subtitle={
-    textAlign:'center',
-    margin:'0px 5px',
-    color:'#99d',
-    fontSize:'24pt',
-    fontWeight:'bold'
-  }
-
-
   return (
     <div>
       <Head>
@@ -34,11 +13,32 @@ export default function Home() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
       </Head>
  
-      <h1 className="bg-primary text-white display-4" style={h1}>React</h1>
+      <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+
+        h2 {
+          text-align: center;
+          margin: 0px 5px;
+          color: #aad;
+          font-size: 36pt;
+          font-weight: bold;
+        }
+
+        p {
+          text-align: left;
+          margin: 0px 5px;
+          color: blue;
+          font-size:18pt;
+        }
+      `}</style>
+
+      <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="my-3" style={subtitle} id ="subtitle">{title}</p>
+        <h2 className="my-3 subtitle">{title}</h2>
         <div className="alert alert-primary text-center">
-          <p className="h5" style={p}>{message}.</p>
+          <p className="h5" >{message}.</p>
         </div>
       </div>
     </div>

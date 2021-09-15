19ccc8a5bacdec67c1246c8bf64cea7d2b3d5fed
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-15 11:47:08 +0900

    L5-3

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -4,7 +4,28 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   let title ="Next.js page";
-  let message = "React Next.js sample page se・yo・ne!.";
+  let message = "React Next.js sample page Da・yo・ne!.";
+
+  const h1 = {
+    textAlign:'right',
+    padding:'5px 15px'
+  }
+
+  const p = {
+    textAlign:'left',
+    margin:'0px 5px',
+    color:'#669',
+    fontSize:'18pt'
+  }
+
+  const subtitle={
+    textAlign:'center',
+    margin:'0px 5px',
+    color:'#99d',
+    fontSize:'24pt',
+    fontWeight:'bold'
+  }
+
 
   return (
     <div>
@@ -13,11 +34,11 @@ export default function Home() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
       </Head>
  
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white display-4" style={h1}>React</h1>
       <div className="container">
-        <h4 className="my-3">{title}</h4>
+        <p className="my-3" style={subtitle} id ="subtitle">{title}</p>
         <div className="alert alert-primary text-center">
-          <p className="h5">{message}.</p>
+          <p className="h5" style={p}>{message}.</p>
         </div>
       </div>
     </div>

cee76def4d15d7bfca59694f97271a48e09e9dfa
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-16 17:01:04 +0900

    5-24 fail...

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,40 +1,25 @@
-// import useSWR from 'swr'
-
-// //fetcher関数の作成
-// const fetcher = url => fetch(url).then(r => r.json())
-
-// export default function index () {
-//   // const { data, error } = useSWR('/api/user', fetcher)
-//   const { data, error } = useSWR('data.json', fetcher)
-//   //エラー
-//   if (error) return <div>failed to load</div>
-//   //ロード中
-//   if (!data) return <div>loading...</div>
-//   //成功
-//   return <pre>{JSON.stringify(data, null, 2)}</pre>
-// }
-
-
+import {useState} from 'react';
 import Layout from '../components/layout';
 import useSWR from 'swr';
 
 export default function Home() {
+  const[ address, setAddress] = useState('/api/hello');
+  const {data, err} = useSWR(address);
 
-  // const fetcher = url => fetch(url).then(r => r.json())
-  // const { data, error } = useSWR('data.json', fetcher)
-  const func = (...args)=> fetch(...args).then(res => res.text());
-  const {data, err} = useSWR('/data.txt', func);
+  const onChange = (e) => {
+    setAddress('/api/hello?id=' + e.target.value);
+  }
 
   return (
     <div>
-      <Layout header="Next.js" title="Top page.">
+      <Layout header="Next.js" title="Top page dayo.">
         <div className="alert alert-primary text-center">
-          <h5 className = "mb-4">
-            {data}
+          <h5 className="mb-4">
+            { JSON.stringify(data) }
           </h5>
+          <input type="number" className="form-control" onChange={onChange} />
         </div>
       </Layout>
     </div>
   )
-
 }

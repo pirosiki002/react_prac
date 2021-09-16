ace619d9b37e1eb951a9e2a7f1115554dc563117
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-16 16:10:00 +0900

    L5-20

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -20,33 +20,21 @@ import useSWR from 'swr';
 
 export default function Home() {
 
-  const fetcher = url => fetch(url).then(r => r.json())
-  // const { data } = useSWR('/data.json');
-  const { data, error } = useSWR('data.json', fetcher)
+  // const fetcher = url => fetch(url).then(r => r.json())
+  // const { data, error } = useSWR('data.json', fetcher)
+  const func = (...args)=> fetch(...args).then(res => res.text());
+  const {data, err} = useSWR('/data.txt', func);
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
-        <div className = "alert alert-primary text-center">
-          <h5 className="mb-4">
-            {data!= undefined ? data.message : 'error...desu....'}
+        <div className="alert alert-primary text-center">
+          <h5 className = "mb-4">
+            {data}
           </h5>
-          <table className="table table-dark">
-            <thead className="">
-              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-            </thead>
-            <tbody>
-              {data != undefined ? data.data.map((value, key)=>(
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <td>{value.mail}</td>
-                  <td>{value.age}</td>
-                </tr>
-              )) : <tr><th></th><td>no data.</td><td></td></tr>}
-            </tbody>
-          </table>
         </div>
       </Layout>
     </div>
   )
+
 }

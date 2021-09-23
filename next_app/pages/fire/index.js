1e3457acaf095f84b32b265371825f9d6a4631b8
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-23 14:23:27 +0900

    L6-8 (firebase8)

--- next_app/pages/fire/index.js
+++ next_app/pages/fire/index.js
@@ -1,54 +1,19 @@
 import {useState, useEffect} from 'react';
 import Layout from '../../components/layout';
-//import firebase from 'firebase';
+import firebase from 'firebase';
 import {collection, getDocs} from "firebase/firestore";
 import {db} from '../../components/fire';
 
-//const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export default function Home() {
-  const mydata = [];
-  const [data, setData] = useState(mydata);
   const [message, setMessage] = useState('wait...');
 
   useEffect(async()=> {
-
-    // db.collection('mydata').get().then((snapshot)=> {
-    //   snapshot.forEach((document)=>{
-    //     const doc = document.data();
-    //     mydata.push(
-    //       // 省略
-    //     )
-    //   })
-    // })
-
-    const querySnapshot = await getDocs(collection(db, "mydata"));
-
-    let mydata = [];
-
-    querySnapshot.forEach((docment) => {
-      console.log(docment.id, " => ", docment.data());
-
-      if (docment.exists()) {
-        console.log("Document data:", docment.data());
-  
-        const doc = docment.data();
-        mydata.push(
-          <tr key={docment.id}>
-            <td><a href={'/fire/del?id=' + docment.id}>{docment.id}</a></td>
-            <td>{doc.name}</td>
-            <td>{doc.mail}</td>
-            <td>{doc.age}</td>
-          </tr>
-        )
-   
-      } else {
-        console.log("No such document!");
-      }    
-    });
-
-    setData(mydata);
-    setMessage('Firebase data.');
+    auth.signInWithPopup(provider).then(result=> {
+      setMessage('logined:' + result.user.displayName)
+    })
   }, [])
 
   return (
@@ -56,19 +21,12 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">{message}</h5>
-          <table className="table bg-white text-left">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data}
-            </tbody>
-          </table>
+          <p className="h6 text-left">
+            uid: {auth.currentUser != null ? auth.currentUser.uid : ''}<br/>
+            displayName: {auth.currentUser != null ? auth.currentUser.displayName : ''}<br/>
+            email: {auth.currentUser != null ? auth.currentUser.email : ''}<br/>
+            phoneNumber: {auth.currentUser != null ? auth.currentUser.phoneNumber : ''}<br/>
+          </p>
         </div>
       </Layout>
     </div>

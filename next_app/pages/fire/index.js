061b4b00e22465ca6c00fb248d5461fee9a3c668
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-09-26 11:09:07 +0900

    L6-10fail

--- next_app/pages/fire/index.js
+++ next_app/pages/fire/index.js
@@ -1,40 +1,84 @@
 import {useState, useEffect} from 'react';
 import Layout from '../../components/layout';
-//import firebase from 'firebase';
-import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'; //firebase9
+import {getAuth, signInWithPopup, GoogleAuthProvider, signOut} from 'firebase/auth'; //firebase9
 import {collection, getDocs} from "firebase/firestore";
 import {db} from '../../components/fire';
 
-//const auth = firebase.auth();
-//const provider = new firebase.auth.GoogleAuthProvider();
+//const db = firebase.firestore();
 const auth = getAuth(); //firebase9
 const provider = new GoogleAuthProvider(); //firebase9
 
+
+//auth.signOut(); //☆彡ログアウトする
+//const [soMessage, soSetMessage] = useState('sign out ready...');
+signOut(auth).then(() => {
+  // Sign-out successful.
+  // soSetMessage('Sign-out successful.');
+  console.log('Sign-out successful.');
+}).catch((error) => {
+  // An error happened.
+  // soSetMessage('Sign-out execing.');
+  console.log('Sign-out execing.');
+});
+
+
 export default function Home() {
+  const mydata = [];
+  const [data, setData] = useState(mydata);
   const [message, setMessage] = useState('wait...');
 
-  useEffect(async()=> {
-    // auth.signInWithPopup(provider).then(result=> {
-    //   setMessage('logined:' + result.user.displayName)
-    // })
+  useEffect(()=> {
     signInWithPopup(auth, provider).then((result) => {
       setMessage('logined:' + result.user.displayName);
     }).catch((error) => {
       //エラー処理
+      setMessage('not logined...');
     });
   }, []);
+
+  useEffect(async()=>{
+    if(auth.currentUser != null) {
+      const querySnapshot = await getDocs(collection(db, "mydata"));
+
+      querySnapshot.forEach((document) => {
+        const doc = document.data();
+        mydata.push(
+          <tr key={document.id}>
+            <td><a href={'/fire/del?id=' + document.id}>{document.id}</a></td>
+            <td>{doc.name}</td>
+            <td>{doc.mail}</td>
+            <td>{doc.age}</td>
+          </tr>
+        )
+      })
+      setData(mydata);
+    }
+    else{
+      mydata.push(
+        <tr key="1"><th colSpan="4">can't get data.</th></tr>
+      )
+    }
+  }, [message])
+
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">{message}</h5>
-          <p className="h6 text-left">
-            uid: {auth.currentUser != null ? auth.currentUser.uid : ''}<br/>
-            displayName: {auth.currentUser != null ? auth.currentUser.displayName : ''}<br/>
-            email: {auth.currentUser != null ? auth.currentUser.email : ''}<br/>
-            phoneNumber: {auth.currentUser != null ? auth.currentUser.phoneNumber : ''}<br/>
-          </p>
+          <table className="table bg-white text-left">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>

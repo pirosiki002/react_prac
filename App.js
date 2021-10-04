386b89acb3fa7d1f1a620770e6e37a826e2a5258
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-10-04 15:44:56 +0900

    L3-17re

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      msg:'Hello Component.',
+    }
+  }
+
   render(){
-    return (
-      <div className = "App">
-        aaaa
+    return <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <p className="subtitle">Show message.</p>
+        <p className="alert alert-warning">{this.state.msg}</p>
+        <p className="alert alert-dark">{this.props.msg}</p>
       </div>
-    );
+    </div>
   }
 }
 

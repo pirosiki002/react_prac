c2a450ac0bf0ee139174de23f7254249141c17af
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-10-11 15:26:00 +0900

    L6-4mod

---
+++ nextjp-prac/app/pages/Fire/index.js
@@ -0,0 +1,72 @@
+// export default function Fire() {
+
+//     return(
+//         <div>
+//             <p>Firebase page</p>
+//         </div>
+//     )
+// }
+
+
+import {useState, useEffect} from 'react';
+import {collection, query, where, doc, getDoc, getDocs} from "firebase/firestore";
+import {db} from '../components/fire';
+
+// export default function Home() {
+export default function Fire() {
+  const mydata = [];
+  const [data, setData] = useState(mydata);
+  const [message, setMessage] = useState('wait...');
+
+  useEffect(async()=> {
+
+    const querySnapshot = await getDocs(collection(db, "mydata"));
+
+    let mydata = [];
+
+    querySnapshot.forEach((docment) => {
+      console.log(docment.id, " => ", docment.data());
+
+      if (docment.exists()) {
+        console.log("Document data:", docment.data());
+        const doc = docment.data();
+        mydata.push(
+          <tr key={docment.id}>
+            <td><a href={'/fire/del?id=' + docment.id}>{docment.id}</a></td>
+            <td>{doc.name}</td>
+            <td>{doc.mail}</td>
+            <td>{doc.age}</td>
+          </tr>
+        )
+      } else {
+        console.log("No such document!");
+      }
+    });
+
+    setData(mydata);
+    setMessage('Firebase data.');
+  }, [])
+
+  return (
+    <div>
+      {/* <Layout header="Next.js" title="Top page."> */}
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">{message}</h5>
+          <table className="table bg-white text-left">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data}
+            </tbody>
+          </table>
+        </div>
+      {/* </Layout> */}
+    </div>
+  )
+}

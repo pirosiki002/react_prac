32901f7a206daed11343174b3f5fc1d6c39c5de7
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-10-11 15:27:28 +0900

    管理画面（仮）

---
+++ nextjp-prac/app/pages/Manage/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';   
+import Link from '@mui/material/Link';
+
+export default function Manage() {
+
+    return (
+        <div>
+            <h1>管理画面</h1>
+            <Link href="./SignIn">SignIn画面に戻る</Link>
+        </div>
+    )
+}
+

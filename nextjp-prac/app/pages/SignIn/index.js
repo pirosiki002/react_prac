764183d73d153e527d8d867298cb355b6f51c88c
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-10-13 10:28:17 +0900

    Firebase連携前

---
+++ nextjp-prac/app/pages/SignIn/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';   
+import SignIn from './SignIn';
+
+//SignInはすでにあるのでSigninで誤魔化し。混乱の元かも
+export default function Signin() {
+  
+    return (
+        // Signin
+        <SignIn></SignIn>
+
+    )
+}  

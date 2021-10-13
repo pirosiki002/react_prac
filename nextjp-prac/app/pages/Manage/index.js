caa8929dbc890973bc6064659733ed7fe3e4382f
Piro <89827750+pirosiki002@users.noreply.github.com>
2021-10-13 14:11:57 +0900

    Firebase連携後　ログインしたらメアド表示

--- nextjp-prac/app/pages/Manage/index.js
+++ nextjp-prac/app/pages/Manage/index.js
@@ -1,17 +1,3 @@
-// import * as React from 'react';
-// import ReactDOM from 'react-dom';
-// import Link from '@mui/material/Link';
-
-// export default function Manage() {
-
-//     return (
-//         <div>
-//             <h1>管理画面</h1>
-//             <Link href="./SignIn">SignIn画面に戻る</Link>
-//         </div>
-//     )
-// }
-
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Link from '@mui/material/Link';
@@ -19,15 +5,48 @@ import Link from '@mui/material/Link';
 import {useState, useEffect} from 'react';
 import {collection, query, where, doc, getDoc, getDocs} from "firebase/firestore";
 import {db} from '../components/fire';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 // export default function Home() {
 export default function Manage() {
   const mydata = [];
   const [data, setData] = useState(mydata);
   const [message, setMessage] = useState('wait...');
+  const [mail, setMail] = useState('mail...');
 
   useEffect(async()=> {
 
+    //現在ログインしているユーザーを取得する
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log('uid =' + uid);
+
+        //ユーザー情報をログで表示
+        if(user !== null) {
+          const displayName = user.displayName;
+          const email = user.email;
+          const uid = user.uid;
+          console.log('Name =' + displayName);
+          console.log('email =' + email);
+          console.log('uid =' + uid);
+
+          setMail(email);
+
+
+        } else {
+          console.log('error user === null');
+        }
+
+      } else {
+
+      }
+    });
+
+
+    //ここのgetDocsをメールアドレスをキーに変えてあげればいけそう。
     const querySnapshot = await getDocs(collection(db, "mydata"));
 
     let mydata = [];
@@ -60,6 +79,7 @@ export default function Manage() {
       {/* <Layout header="Next.js" title="Top page."> */}
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">{message}</h5>
+          <h5 className="mb-4">{mail}</h5>
           <table className="table bg-white text-left">
             <thead>
               <tr>
